3de7c3a99f5db7d9bf3487c4377aa9c168c36915
MABB <michaelapril81416@gmail.com>
2025-12-05 21:32:10 +0800

    Add files via upload
    This is the official database

---
+++ airline_system.sql
@@ -0,0 +1,303 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2025 at 02:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `airline_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aircrafts`
+--
+
+CREATE TABLE `aircrafts` (
+  `aircraft_id` int(11) NOT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `status` enum('Active','In-Maintenance') DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `aircrafts`
+--
+
+INSERT INTO `aircrafts` (`aircraft_id`, `model`, `status`) VALUES
+(1, 'Boeing 737', 'Active'),
+(2, 'Airbus A320', 'In-Maintenance');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `booking_id` int(11) NOT NULL,
+  `pnr` varchar(6) DEFAULT NULL,
+  `flight_id` int(11) DEFAULT NULL,
+  `passenger_id` int(11) DEFAULT NULL,
+  `seat_class` enum('Economy','Business') DEFAULT NULL,
+  `seat_number` varchar(5) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `status` enum('Confirmed','Cancelled') DEFAULT 'Confirmed'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`booking_id`, `pnr`, `flight_id`, `passenger_id`, `seat_class`, `seat_number`, `price`, `status`) VALUES
+(1, 'LBB2SO', 1, 2, 'Business', '10', 15000.00, 'Confirmed'),
+(3, 'EGSNEZ', 1, 2, 'Business', '10', 15000.00, 'Confirmed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkin`
+--
+
+CREATE TABLE `checkin` (
+  `checkin_id` int(11) NOT NULL,
+  `booking_id` int(11) DEFAULT NULL,
+  `bags_checked` int(11) DEFAULT NULL,
+  `boarding_time` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `checkin`
+--
+
+INSERT INTO `checkin` (`checkin_id`, `booking_id`, `bags_checked`, `boarding_time`) VALUES
+(1, 3, 2, '2025-12-05 21:10:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crew`
+--
+
+CREATE TABLE `crew` (
+  `crew_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `role` enum('Pilot','Co-Pilot','Cabin Crew') DEFAULT NULL,
+  `status` enum('Available','On-Duty','Resting') DEFAULT 'Available'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `crew`
+--
+
+INSERT INTO `crew` (`crew_id`, `name`, `role`, `status`) VALUES
+(1, 'Capt. Ri', 'Pilot', 'Available'),
+(2, 'Sarah Lin', 'Cabin Crew', 'Available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flights`
+--
+
+CREATE TABLE `flights` (
+  `flight_id` int(11) NOT NULL,
+  `flight_number` varchar(10) DEFAULT NULL,
+  `origin` varchar(50) DEFAULT NULL,
+  `destination` varchar(50) DEFAULT NULL,
+  `flight_date` date DEFAULT NULL,
+  `status` enum('Scheduled','Delayed','Cancelled','Arrived') DEFAULT 'Scheduled',
+  `aircraft_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `flights`
+--
+
+INSERT INTO `flights` (`flight_id`, `flight_number`, `origin`, `destination`, `flight_date`, `status`, `aircraft_id`) VALUES
+(1, 'PR101', 'Manila', 'Cebu', '2025-12-25', 'Scheduled', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flight_crew`
+--
+
+CREATE TABLE `flight_crew` (
+  `assignment_id` int(11) NOT NULL,
+  `flight_id` int(11) DEFAULT NULL,
+  `crew_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passengers`
+--
+
+CREATE TABLE `passengers` (
+  `passenger_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `loyalty_points` int(11) DEFAULT 0,
+  `tier` enum('Blue','Silver','Gold') DEFAULT 'Blue'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `passengers`
+--
+
+INSERT INTO `passengers` (`passenger_id`, `name`, `email`, `phone`, `loyalty_points`, `tier`) VALUES
+(1, 'Juan Cruz', 'juan@email.com', NULL, 500, 'Silver'),
+(2, 'Michael April Boquiron', 'Michael@gmai.com', NULL, 3000, 'Blue'),
+(3, '', 'Michael@gmail.com', NULL, 0, 'Blue');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aircrafts`
+--
+ALTER TABLE `aircrafts`
+  ADD PRIMARY KEY (`aircraft_id`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD UNIQUE KEY `pnr` (`pnr`),
+  ADD KEY `flight_id` (`flight_id`),
+  ADD KEY `passenger_id` (`passenger_id`);
+
+--
+-- Indexes for table `checkin`
+--
+ALTER TABLE `checkin`
+  ADD PRIMARY KEY (`checkin_id`),
+  ADD KEY `booking_id` (`booking_id`);
+
+--
+-- Indexes for table `crew`
+--
+ALTER TABLE `crew`
+  ADD PRIMARY KEY (`crew_id`);
+
+--
+-- Indexes for table `flights`
+--
+ALTER TABLE `flights`
+  ADD PRIMARY KEY (`flight_id`),
+  ADD UNIQUE KEY `flight_number` (`flight_number`),
+  ADD KEY `aircraft_id` (`aircraft_id`);
+
+--
+-- Indexes for table `flight_crew`
+--
+ALTER TABLE `flight_crew`
+  ADD PRIMARY KEY (`assignment_id`),
+  ADD KEY `flight_id` (`flight_id`),
+  ADD KEY `crew_id` (`crew_id`);
+
+--
+-- Indexes for table `passengers`
+--
+ALTER TABLE `passengers`
+  ADD PRIMARY KEY (`passenger_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aircrafts`
+--
+ALTER TABLE `aircrafts`
+  MODIFY `aircraft_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `checkin`
+--
+ALTER TABLE `checkin`
+  MODIFY `checkin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `crew`
+--
+ALTER TABLE `crew`
+  MODIFY `crew_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `flights`
+--
+ALTER TABLE `flights`
+  MODIFY `flight_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `flight_crew`
+--
+ALTER TABLE `flight_crew`
+  MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `passengers`
+--
+ALTER TABLE `passengers`
+  MODIFY `passenger_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`flight_id`) REFERENCES `flights` (`flight_id`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`passenger_id`) REFERENCES `passengers` (`passenger_id`);
+
+--
+-- Constraints for table `checkin`
+--
+ALTER TABLE `checkin`
+  ADD CONSTRAINT `checkin_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `bookings` (`booking_id`);
+
+--
+-- Constraints for table `flights`
+--
+ALTER TABLE `flights`
+  ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`aircraft_id`) REFERENCES `aircrafts` (`aircraft_id`);
+
+--
+-- Constraints for table `flight_crew`
+--
+ALTER TABLE `flight_crew`
+  ADD CONSTRAINT `flight_crew_ibfk_1` FOREIGN KEY (`flight_id`) REFERENCES `flights` (`flight_id`),
+  ADD CONSTRAINT `flight_crew_ibfk_2` FOREIGN KEY (`crew_id`) REFERENCES `crew` (`crew_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
